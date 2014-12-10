cc3393d912bb6ac50533000c1460c79d24b9fe5f
Geoff Buesing <gbuesing@gmail.com>
2014-12-10 17:03:53 -0600

    cleanup

--- app.rb
+++ app.rb
@@ -24,29 +24,15 @@ class DigitClassifierApp < Sinatra::Application
       normalize = -> (val, fromLow, fromHigh, toLow, toHigh) {  (val - fromLow) * (toHigh - toLow) / (fromHigh - fromLow).to_f }
 
       canvas = ChunkyPNG::Canvas.from_data_url(dataURL)
-      # canvas.grayscale!
-      # canvas.trim!
       canvas.resample_bilinear!(28,28)
-      # canvas.border! 4, 0
-      # canvas.save 'test.png'
-
-      newpng = ChunkyPNG::Image.new(28, 28)
 
       pixels = []
       28.times do |y| 
         28.times {|x| pixels << canvas[x, y] }
       end
+      
       max, min = pixels.max, pixels.min
       pixels = pixels.map {|p| normalize.(p, min, max, 0, 1) }
-
-      28.times do |y|
-        x_offset = y * 28
-        28.times {|x| newpng[x,y] = ChunkyPNG::Color("black @ #{pixels[x + x_offset]}") }
-      end
-
-      # newpng.save 'test2.png'
-
-
       pixels
     end
 
